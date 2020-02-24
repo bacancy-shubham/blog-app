27dd9e9f09c9709dc2903b5a5a00b6ef3d4f5302
bacancy-shubham <shubham.chandroliya@bacancy.com>
2020-02-24 18:44:50 +0530

    add delete feature for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,24 @@
 class CommentsController < ApplicationController
+   before_action :set_article
   def create
-    @article = Article.find(params[:article_id])
     @comments = @article.article_comments.new(comment_param)
     @comments.user = current_user
     @comments.save!
     redirect_to article_path(@article)
   end 
 
+  def destroy
+    @comments = @article.article_comments.find(params[:id])
+    @comments.destroy
+    flash[:notice] = "comment as successfully deleted"
+    redirect_to article_path(@article)
+
+  end
+
     private
+      def set_article
+          @article = Article.find(params[:article_id])
+      end
       def comment_param
         params.require(:article_comment).permit(:comment)
       end
